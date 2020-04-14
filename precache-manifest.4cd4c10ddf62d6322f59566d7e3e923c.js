567d6c842bdd2a7954dda86e8608a43c2402e8e2
mohit9222 <mohit.jan92@gmail.com>
2020-04-14 21:29:51 +0530

    Updates

---
+++ precache-manifest.4cd4c10ddf62d6322f59566d7e3e923c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d63902db5e5672b8b707f7488efe6e",
+    "url": "/Test-Portfolio/index.html"
+  },
+  {
+    "revision": "1e8fee0b346382cad9a1",
+    "url": "/Test-Portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "16018ce5416687ca5cae",
+    "url": "/Test-Portfolio/static/css/main.30bdad60.chunk.css"
+  },
+  {
+    "revision": "1e8fee0b346382cad9a1",
+    "url": "/Test-Portfolio/static/js/2.15264801.chunk.js"
+  },
+  {
+    "revision": "4f2869121f2955e075058deaae0251dd",
+    "url": "/Test-Portfolio/static/js/2.15264801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16018ce5416687ca5cae",
+    "url": "/Test-Portfolio/static/js/main.800845a9.chunk.js"
+  },
+  {
+    "revision": "07b778a9e77adb743de1",
+    "url": "/Test-Portfolio/static/js/runtime-main.37cabfa1.js"
+  }
+]);
